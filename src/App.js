9eb8f1f85cb6be50ca81f7736f6833e2d24e6b33
Hatice Betul <hatbet07@gmail.com>
2022-08-07 23:11:59 +0300

    react bootstrap, styled components, tailwind  özellikleri eklendi, process ve development alanlarını ayrıştırılması için gerekli işlemler yapıldı.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import './App.css';
+import { useEffect } from 'react';
+import './style.css'
+import Test from './Test';
+import { Title } from './Components';
+import './tailwind.css';
 
 function App() {
+  useEffect(() => {
+    if(process.env.NODE_ENV === 'production') {
+
+    }
+  }, [])
+
   return (
     <div className="App">
-test    </div>
+      <Title>{process.env.NODE_ENV}</Title>
+      <Title theme="dark">{process.env.REACT_APP_API_URL}</Title>
+      <Test></Test>
+    </div>
   );
 }
 
---
+++ src/Bootstrap.js
@@ -0,0 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function Bootstrap (){
+    return(
+        <div>
+            
+        </div>
+    )
+}
