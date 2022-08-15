258810387953497b8a0f0915687014c5999f2096
Hatice Betul <hatbet07@gmail.com>
2022-08-15 21:57:29 +0300

    ders6 UseEffect işlemleri , api çağırma işlemleri

--- src/Test.js
+++ src/Test.js
@@ -2,29 +2,39 @@ import React, { useEffect, useState } from 'react'
 
 export default function Test() {
 
-  const [count, setCount] = useState(0)
+  const [postId, setPostId] = useState(1)
+  const [post, setPost] = useState(false)
+
   useEffect(() => {
     console.log('Component ilk yüklendiğinde çalıştı, ComponentDidMount')
-
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => response.json())
+    .then(json => console.log(json))
+    let Interval = setInterval(() => console.log('Interval çalıştı'), 1000)
     return() => {
       console.log('componentUnMount')
+      clearInterval(Interval)
     }
   }, [])
 
   useEffect(()=>{
-    console.log('count değeri değişti =' , count)
-  }, [count])
+    fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+    .then(res => res.json())
+    .then(data => setPost(data))
+  }, [postId])
    
-  useEffect (() => {
-    console.log('component render oldu, ComponentDidUpdate')
-  })
+  // useEffect (() => {
+  //   console.log('component render oldu, ComponentDidUpdate')
+  // })
+
   return (
     <div>
-      <h3> {count}</h3>
-      <button onClick={() => setCount(count => count + 1 )}>Arttır </button>
+      <h3> {postId}</h3>
+      
+      <button onClick={() => setPostId(postId => postId + 1 )}>Sonraki Konu </button>
       <hr/>
       Test
       
     </div>
-  )
+  );
 }
