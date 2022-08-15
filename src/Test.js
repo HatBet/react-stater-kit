46aaa6592dde6888e27d4d6576dad27c715ea652
Hatice Betul <hatbet07@gmail.com>
2022-08-15 21:28:48 +0300

    ders6

--- src/Test.js
+++ src/Test.js
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 export default function Test() {
+
+  useEffect(() => {
+    console.log('Component ilk yüklendiğinde çalıştı')
+  }, [])
+   
+  useEffect (() => {
+    console.log('component render oldu')
+  })
   return (
     <div>Test</div>
   )
